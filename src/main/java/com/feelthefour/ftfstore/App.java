37d8007396503f31e9ddc771a7509370d0119429
Jarrett Dougherty <jarrettdougherty@gmail.com>
2025-02-06 14:02:01 -0500

    connect frontend static files

--- src/main/java/com/feelthefour/ftfstore/App.java
+++ src/main/java/com/feelthefour/ftfstore/App.java
@@ -1,6 +1,9 @@
 package com.feelthefour.ftfstore;
 
+import java.nio.file.Paths;
+
 import io.javalin.Javalin;
+import io.javalin.http.staticfiles.Location;
 
 /**
  * Hello world!
@@ -9,8 +12,11 @@ import io.javalin.Javalin;
 public class App {
     public static void main( String[] args ) {
         System.out.println( "Hello World!" );
-        var app = Javalin.create()
-            .get("/", ctx -> ctx.result("Hello World 2"))
+        // System.out.println("PATH!!!!: " + Paths.get("public").toAbsolutePath().toString());
+        var app = Javalin.create(config -> {
+            config.staticFiles.add("public", Location.EXTERNAL);
+        })
+            // .get("/", ctx -> ctx.result("Hello World 2"))
             .start(7070);
     }
 }
