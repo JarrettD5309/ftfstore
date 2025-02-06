5518b1c6d3591395c8f56e838f0cb8ad0c5ea2cf
Jarrett Dougherty <jarrettdougherty@gmail.com>
2025-02-06 15:05:23 -0500

    connect to stripe

--- src/main/java/com/feelthefour/ftfstore/App.java
+++ src/main/java/com/feelthefour/ftfstore/App.java
@@ -1,8 +1,13 @@
 package com.feelthefour.ftfstore;
 
-import java.nio.file.Paths;
+// import java.nio.file.Paths;
+
+import com.stripe.Stripe;
+import com.stripe.model.checkout.Session;
+import com.stripe.param.checkout.SessionCreateParams;
 
 import io.javalin.Javalin;
+import io.javalin.http.HttpStatus;
 import io.javalin.http.staticfiles.Location;
 
 /**
@@ -10,13 +15,42 @@ import io.javalin.http.staticfiles.Location;
  *
  */
 public class App {
-    public static void main( String[] args ) {
-        System.out.println( "Hello World!" );
-        // System.out.println("PATH!!!!: " + Paths.get("public").toAbsolutePath().toString());
-        var app = Javalin.create(config -> {
-            config.staticFiles.add("public", Location.EXTERNAL);
-        })
-            // .get("/", ctx -> ctx.result("Hello World 2"))
-            .start(7070);
-    }
+	private static int PORT = 7070;
+
+	public static void main(String[] args) {
+		// This is your test secret API key.
+		Stripe.apiKey = "";
+
+		var app = Javalin.create(config -> {
+			config.staticFiles.add("public", Location.EXTERNAL);
+		});
+
+		app.post("/create-checkout-session", cxt -> {
+			String priceID = cxt.formParam("priceID");
+			Long quantity = Long.valueOf(cxt.formParam("quantity"));
+
+			String YOUR_DOMAIN = "http://localhost:" + PORT;
+			SessionCreateParams params = SessionCreateParams.builder()
+					.setMode(SessionCreateParams.Mode.PAYMENT)
+					.setSuccessUrl(YOUR_DOMAIN + "/success.html")
+					.setCancelUrl(YOUR_DOMAIN + "/cancel.html")
+					.setAutomaticTax(
+							SessionCreateParams.AutomaticTax.builder()
+									.setEnabled(true)
+									.build())
+					.addLineItem(
+							SessionCreateParams.LineItem.builder()
+									.setQuantity(quantity)
+									// Provide the exact Price ID (for example, pr_1234) of the product you want to
+									// sell
+									.setPrice(priceID)
+									.build())
+					.build();
+			Session session = Session.create(params);
+
+			cxt.redirect(session.getUrl(), HttpStatus.SEE_OTHER);
+		});
+
+		app.start(PORT);
+	}
 }
